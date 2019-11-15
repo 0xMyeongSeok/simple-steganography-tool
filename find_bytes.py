ddd36613427d523c30d186f1ffafd32fb5cd0414
M.S. Song <songblue61@naver.com>
2019-11-15 13:49:47 +0900

    버그 수정 및 메서드 이름 변경
    버그 수정
    - 버퍼 크기에 따라 marker가 잘리는 경우에 정상 동작하지 않는 버그 수정
    메서드 이름 변경
    - 'find'보다 'search'가 메서드의 기능에 적합
    - 파일의 끝부터 탐색하는 메서드는 '_r' 접미사를 붙임

--- find_bytes.py
+++ find_bytes.py
@@ -1,18 +1,20 @@
 import os
 
 
-def find_bytes(file_path, sig, buffer_size=1024):
-    """Function that find specific bytes, and then return offset.
+def search_bytes_r(file_path, marker, buffer_size=1024):
+    """
+    Function that search specific bytes(marker) from the end of a file,
+    and then return start offset of the bytes.
 
     Arguments:
         file_path {file}
-        sig {bytes} -- specific bytes to find.
+        marker {bytes} -- specific bytes to find. e.g. b'\\xff\\xd9'
     
     Keyword Arguments:
         buffer_size {int} -- (default: {1024})
 
     Returns:
-        int -- if found, return start offset of bytes. else, return -1.
+        int -- if found, return start offset of the bytes. else, return -1.
     """
     
     with open(file_path, "rb") as f:
@@ -26,13 +28,22 @@ def find_bytes(file_path, sig, buffer_size=1024):
             # 버퍼에 데이터 저장 및 탐색
             buffer = f.read(min(remaining_size, buffer_size))
             remaining_size -= buffer_size
-            idx = buffer.find(sig)
-            # sig를 찾았다면 위치 offset 반환
+            idx = buffer.find(marker)
+            # marker를 못찾았다면,
+            # 버퍼 크기에 따라 marker가 잘리는 경우도 있으므로 확인해야 함
+            if(idx == -1):
+                offset = offset + len(marker) - 1
+                if(offset < file_size):
+                    f.seek(-offset, os.SEEK_END)
+                    buffer = f.read(len(marker)*2 - 2)
+                    idx = buffer.find(marker)
+
+            # marker를 찾았다면 위치 offset 반환
             if(idx != -1):
                 return file_size - offset + idx
 
         return -1
 
 
-if __name__ == "__main__":            
-    print(find_bytes("./test.jpg", b"\xff\xd9"))
+if __name__ == "__main__":
+    print(search_bytes_r("./image01.jpg", b"\x3A\x5E\x3C\xB2"))
