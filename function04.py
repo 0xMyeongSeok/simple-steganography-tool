ebfb5e76ba9472eec2d227e833b938def268e021
jesus0084 <jesus0084@naver.com>
2019-11-24 21:29:05 +0900

    기능4 수정
    null file exception detect 기능 추가

--- function04.py
+++ function04.py
@@ -6,36 +6,45 @@ def function04():
     file1_path = input("파일 1 경로 입력 >> ")
     file2_path = input("파일 2 경로 입력 >> ")
 
-    myFile1 = open(file1_path, mode = 'rb')
-    myFile2 = open(file2_path,mode = 'rb')
-    string1 = myFile1.read()
-    string2 = myFile2.read()
+    try:
+        myFile1 = open(file1_path, mode = 'rb')
+        myFile2 = open(file2_path,mode = 'rb')
+        string1 = myFile1.read()
+        string2 = myFile2.read()
 
-    len1 = len(string1)
-    len2 = len(string2)
-    base = 0
+        len1 = len(string1)
+        len2 = len(string2)
+        base = 0
 
-    if len1>len2:
-        base=len1
-        for pos in range(0,len1-len2):
-            string2 += b'@'
-        print("경고! 파일 1의 크기가 더 크므로 파일 2를 @으로 패딩했습니다")
+        if len1>len2:
+            base=len1
+            for pos in range(0,len1-len2):
+                string2 += b'@'
+            print("경고! 파일 1의 크기가 더 크므로 파일 2를 @으로 패딩했습니다")
 
-    elif len1<len2:
-        base=len2
-        for pos in range(0,len2-len1):
-            string1+=b'@'
-        print("경고! 파일 2의 크기가 더 크므로 파일 1을 @으로 패딩했습니다")
-    elif len1==len2:
-        base=len1
+        elif len1<len2:
+            base=len2
+            for pos in range(0,len2-len1):
+                string1+=b'@'
+            print("경고! 파일 2의 크기가 더 크므로 파일 1을 @으로 패딩했습니다")
+        elif len1==len2:
+            base=len1
 
-    for pos in range(0,base):
-        if string1[pos] == string2[pos]:
-            continue
-        elif string1[pos] != string2[pos]:
-            print("인덱스 :", pos, ", 파일1 내용 :",  chr(string1[pos]),", 파일2 내용 :",chr(string2[pos]))
+        for pos in range(0,base):
+            if string1[pos] == string2[pos]:
+                continue
+            elif string1[pos] != string2[pos]:
+                print("인덱스 :", pos, ", 파일1 내용 :",  chr(string1[pos]),", 파일2 내용 :",chr(string2[pos]))
 
-    print("비교 끝")
-    myFile1.close()
-    myFile2.close()
-    return 0
+        print("비교 끝")
+        myFile1.close()
+        myFile2.close()
+        return 0
+        
+    except FileNotFoundError:
+        print("[!] 해당하는 파일이 없습니다.", file=sys.stderr)
+        return -1
+
+    except PermissionError:
+        print("[!] Permission denied. 해당 파일이 열려있는지 확인하세요.", file=sys.stderr)
+        return -1
