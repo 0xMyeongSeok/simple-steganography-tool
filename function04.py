85ed937f02a6f72e4fe38533cd60c676e74610f6
Bob <bob@gmail.com>
2019-11-29 23:33:56 +0900

    code formatting using 'black'

--- function04.py
+++ function04.py
@@ -2,13 +2,14 @@ import binascii
 import sys
 import os
 
-def function04():   
+
+def function04():
     file1_path = input("파일 1 경로 입력 >> ")
     file2_path = input("파일 2 경로 입력 >> ")
 
     try:
-        myFile1 = open(file1_path, mode = 'rb')
-        myFile2 = open(file2_path,mode = 'rb')
+        myFile1 = open(file1_path, mode="rb")
+        myFile2 = open(file2_path, mode="rb")
         string1 = myFile1.read()
         string2 = myFile2.read()
 
@@ -16,31 +17,38 @@ def function04():
         len2 = len(string2)
         base = 0
 
-        if len1>len2:
-            base=len1
-            for pos in range(0,len1-len2):
-                string2 += b'@'
+        if len1 > len2:
+            base = len1
+            for pos in range(0, len1 - len2):
+                string2 += b"@"
             print("경고! 파일 1의 크기가 더 크므로 파일 2를 @으로 패딩했습니다")
 
-        elif len1<len2:
-            base=len2
-            for pos in range(0,len2-len1):
-                string1+=b'@'
+        elif len1 < len2:
+            base = len2
+            for pos in range(0, len2 - len1):
+                string1 += b"@"
             print("경고! 파일 2의 크기가 더 크므로 파일 1을 @으로 패딩했습니다")
-        elif len1==len2:
-            base=len1
+        elif len1 == len2:
+            base = len1
 
-        for pos in range(0,base):
+        for pos in range(0, base):
             if string1[pos] == string2[pos]:
                 continue
             elif string1[pos] != string2[pos]:
-                print("인덱스 :", pos, ", 파일1 내용 :",  chr(string1[pos]),", 파일2 내용 :",chr(string2[pos]))
+                print(
+                    "인덱스 :",
+                    pos,
+                    ", 파일1 내용 :",
+                    chr(string1[pos]),
+                    ", 파일2 내용 :",
+                    chr(string2[pos]),
+                )
 
         print("비교 끝")
         myFile1.close()
         myFile2.close()
         return 0
-        
+
     except FileNotFoundError:
         print("[!] 해당하는 파일이 없습니다.", file=sys.stderr)
         return -1
--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def print_menu():
     print("""
 ----------------------------------------------
 1) <기능1> JPG 파일의 EOI 이후에 data 삽입하기
-2) 기능1 적용된 파일 복구하기
+2) 기능1에 의해 숨겨진 데이터 추출하기
 3) <기능2> JPG 파일의 헤더 이후에 data 삽입하기
-4) 기능2 적용된 파일 복구하기
-5) 기능3 -현재 미구현-
-6) 기능4 두 파일에서 다른 부분 구하기
+4) 기능2에 의해 숨겨진 데이터 추출하기
+5) <기능3> BMP 파일에 데이터 숨기기 (LSB steganography)
+6) 기능3에 의해 숨겨진 데이터 추출하기
+7) <기능4> 두 파일에서 다른 부분 구하기
 0) 종료
 ----------------------------------------------""")
 
@@ -21,7 +22,7 @@ if __name__ == "__main__" :
         print_menu()
         try:
             choice = int(input("원하시는 기능의 번호를 입력하세요: "))
-            if choice not in [1,2,3,4,5,6,0]:
+            if choice not in [1,2,3,4,5,6,7,0]:
                 raise ValueError
 
             if choice == 1:
@@ -37,10 +38,13 @@ if __name__ == "__main__" :
                 function02_recover()
 
             elif choice == 5:
-                break
+                pass
 
             elif choice == 6:
                 function04()
+            
+            elif choice == 7:
+                pass
 
             elif choice == 0:
                 break
